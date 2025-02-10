f1161ff48ad89d2b337ae694d559a43c87c8fea2
branliyan <liyanage.brandon27@gmail.com>
2025-02-09 20:17:49 -0500

    Finished Lab04

---
+++ lab02/index.php
@@ -0,0 +1,21 @@
+<?php
+
+     $array = array("Art1", "Art2", "Art3");
+     $len = sizeof($array);
+     echo "<h1>Art Work Database</h2>";
+     echo "<table>";
+     echo "<tr>";
+     echo "<td>Name</td>";
+     echo "</tr>";
+     echo "</table>";
+     echo "<p>Size of Collection: $len </p>";
+     
+     for ($i=0;$i<$len;$i++){
+          echo "<p>hello</p>";
+     }
+
+?>
+
+
+
+
