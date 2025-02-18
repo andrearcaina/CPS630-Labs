34310cc811b990447154cad3cb95a450acf6fd2b
branliyan <liyanage.brandon27@gmail.com>
2025-02-18 14:08:21 -0500

    UI changes

--- project/iter1&2/pages/services.php
+++ project/iter1&2/pages/services.php
@@ -3,14 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/styles/about.css">
     <link rel="stylesheet" href="../public/styles/global.css">
-    <title>About Us</title>
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Playwrite+IT+Moderna:wght@100..400&display=swap" rel="stylesheet">
+
+    <title>Services</title>
 </head>
-<body>  
+<body>
+  
 <?php include '../components/header.php'; ?>
 
-Services
+  <center>
+      <h1 style="font-family: Playwrite IT Moderna, serif; font-weight: 800; font-style: normal; font-size: 45px">Our Services</h1>
+  </center>
+  <div style="display: flex; max-width: 1400px; margin: auto; margin-top: 75px">
 
-<?php include '../components/footer.php'; ?>
+
+  <div class="frame">
+    <h1>Andre</h1>
+    <img src="../public/images/andre.png" alt="Logo" class="portrait">
+    <p>Hi There my name is Andre and I'm a third-year Computer Science student at TMU!</p>
+  </div>
+
+  <div class="frame">
+    <h1>Brandon</h1>
+    <img src="../public/images/brandon.png" alt="Logo" class="portrait">
+    <p>Hi There my name is Brandon and I'm a third-year Computer Science student at TMU!</p>
+  </div>
+
+  <div class="frame">
+    <h1>Felipe</h1>
+    <img src="../public/images/felipe.png" alt="Logo" class="portrait">
+    <p>Hi There my name is Felipe and I'm a third-year Computer Science student at TMU!</p>
+  </div>
+
+  </div>
+  
+  <?php include '../components/footer.php'; ?>
+
 </body>
 </html>
